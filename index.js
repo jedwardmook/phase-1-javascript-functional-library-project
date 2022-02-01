8bfb2e1fa3525fceb34ccb8ec7d83324102d6f1d
John Mook <jedwardmook@yahoo.com>
2022-02-01 13:52:37 -0600

    commit tree 07d54b775eda348f331c725e2805675b6e5f5636

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function myEach(collection, alert){
+    for(let i = 0; i < collection.length; i++){
+        alert(collection[i])
+        } if(collection === Object){
+            alert(Object.values[i])
+        }
+        return collection
+    };
